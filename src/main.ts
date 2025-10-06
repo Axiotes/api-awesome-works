129719dd7bc4ba4dae03f9c290607a13a2c2173c
Arthur Axiotes <axiotesarthur@gmail.com>
2025-10-06 12:24:23 -0300

    chore: config do main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.setGlobalPrefix('api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('Awesome Works API')
+    .setDescription(
+      `API para controle e rastreamento de equipamentos de TI, permitindo uma melhor gestão dos equipamentos tecnológicos da organização.`,
+    )
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = (): OpenAPIObject =>
+    SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
