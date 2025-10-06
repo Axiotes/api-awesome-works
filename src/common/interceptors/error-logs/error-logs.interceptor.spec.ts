e2f86f5e34a29b4b78d025c9dc02d3f9ba071083
Arthur Axiotes <axiotesarthur@gmail.com>
2025-10-06 13:50:54 -0300

    chore: interceptor para geracao de logs gerais

---
+++ src/common/interceptors/error-logs/error-logs.interceptor.spec.ts
@@ -0,0 +1,7 @@
+import { ErrorLogsInterceptor } from './error-logs.interceptor';
+
+describe('ErrorLogsInterceptor', () => {
+  it('should be defined', () => {
+    expect(new ErrorLogsInterceptor()).toBeDefined();
+  });
+});
