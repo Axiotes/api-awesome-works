68504a21f8391dcac3ec8a1ef681a2f179c861c5
Arthur Axiotes <axiotesarthur@gmail.com>
2025-10-06 23:37:20 -0300

    feat: cricao de controller, service e repository de setores

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServicesModule } from './common/services/services.module';
 import { EquipamentModule } from './modules/equipament/equipament.module';
+import { DepartmentModule } from './modules/department/department.module';
 
 @Module({
-  imports: [ServicesModule, EquipamentModule],
+  imports: [ServicesModule, EquipamentModule, DepartmentModule],
   controllers: [AppController],
   providers: [AppService],
 })
